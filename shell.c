535c035891c71bbe31a417349986fac184177fb5
Bayan Berri <bberri@stuy.edu>
2017-11-22 19:50:17 -0500

    So right now I've implemented fgets and printing out the shell command prompt $ and the current working directory. I also think we should move the parsing to another functionmake run

--- shell.c
+++ shell.c
@@ -10,34 +10,40 @@
 
 int parse(){
 
+  /*
+    I think this should be moved to parse()
+
+    if (!fork()) execlp("clear", "clear", NULL);
+
+    char input[256];
+    char token[256]
+  
+    scanf("%s", input);
+
+    while (token = strsep(&input, ";"));
+  
+    return 0;
+  */
+
   return 0;
 }
 
 
 /*main will call all other functions.
  * So it will print a shell line for inputs
+ * and then call all the other functions that we end up writing. 
  */
 int main(){
   
-  char c[500];//input command
+  char command_input[500];
   char * cmd[100];//commands string
   char workingdir[500];//current working directory
 
   getcwd(workingdir,sizeof(workingdir));//(man getcwd for info) it gets current working directory
   printf("C-SHELL... %s $ ",workingdir);
-  
+  //user input part:
+  fgets(command_input,sizeof(command_input),stdin);//(destination,bytes,file pointer)
+
   return 0;
 }
-  /*
-  if (!fork()){
-    execlp("clear", "clear", NULL);
-  }
-  char input[256];
-  char token[256];
-  scanf("%s", input);
-
-  while (token = strsep(&input, ";"));
   
-  return 0;
-
-  */
