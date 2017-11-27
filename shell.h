14ff3bf4887d72df1a9b040ec44a4b848a3dc0ca
Bayan Berri <bberri@stuy.edu>
2017-11-27 00:02:54 -0500

    changed headers

--- shell.h
+++ shell.h
@@ -8,12 +8,19 @@
 #include <sys/types.h>
 #include <sys/wait.h>
 
-char ** parse_args(char* line, char ** retans);
+
 char ** split_line(char* line, char* character, char* split_input, char ** retans);
-void execute(char** parsed_line);
-char** parse_multiple_commands(char* line, char ** retans);
+
 void replace_str(char*line,char*substring,char*replace_with);
+
+void execute(char** parsed_line);
+
+void cd(char** parsed_line);
+
 char** run_the_shell(char * command_input, char * input, char ** cmd);
-void print_dir();
+
 void check_exit(char* token);
+
+void print_dir();
+
 void main();
