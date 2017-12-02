71df59dad8fd0a3921c2e21329e04fad86ab29e9
BrianLim167 <blim167@gmail.com>
2017-12-01 22:51:04 -0500

    fixed restoring stdin / stdout

--- shell.c
+++ shell.c
@@ -1,11 +1,11 @@
 #include "shell.h"
 
 /**
- * @Function: split_line
- * @Params: char* line, char * character,char* split_input, char** retans
- * @Returns: char**
- * @Explanation: splits the line at the character. If multiple occurrences each will be a token in retans.
- */
+* @Function: split_line
+* @Params: char* line, char * character,char* split_input, char** retans
+* @Returns: char**
+* @Explanation: splits the line at the character. If multiple occurrences each will be a token in retans.
+*/
 
 char ** split_line(char* line, char* character, char* split_input, char ** retans) {
   int i=0;
@@ -20,11 +20,11 @@ char ** split_line(char* line, char* character, char* split_input, char ** retan
 
 
 /**
- * @Function: replace_str
- * @Params: char* line, char* substring, char* replace_with
- * @Returns: Nothing
- * @Explanation: replaces the substring with replace_with. Used for if user surrounds semicolon with spaces.
- */
+* @Function: replace_str
+* @Params: char* line, char* substring, char* replace_with
+* @Returns: Nothing
+* @Explanation: replaces the substring with replace_with. Used for if user surrounds semicolon with spaces.
+*/
 void replace_str(char* line, char* substring, char* replace_with){
   char buffer[100];
   char*p=line;
@@ -39,11 +39,11 @@ void replace_str(char* line, char* substring, char* replace_with){
 }
 
 /**
- * @Function: execute
- * @Params: char** parsed_line
- * @Returns: Nothing
- * @Explanation: forks, parent waits for child to execute parsed_line
- */
+* @Function: execute
+* @Params: char** parsed_line
+* @Returns: Nothing
+* @Explanation: forks, parent waits for child to execute parsed_line
+*/
 void execute(char **parsed_line) {
   //printf("@@%s\n", parsed_line[0]);
   int f = fork();
@@ -58,11 +58,11 @@ void execute(char **parsed_line) {
 }
 
 /**
- * @Function: cd
- * @Params: char ** parsed_line
- * @Returns: nothing
- * @Explanation: change directory function, includes cd with no param and cd ~ as well
- */
+* @Function: cd
+* @Params: char ** parsed_line
+* @Returns: nothing
+* @Explanation: change directory function, includes cd with no param and cd ~ as well
+*/
 void cd(char** parsed_line){
   if(strcmp(parsed_line[0],"cd") == 0){
     if( strcmp(parsed_line[1],"~")==0){
@@ -79,11 +79,11 @@ void cd(char** parsed_line){
 
 
 /**
- * @Function: run_the_shell
- * @Params: char* command_input, char* split_input, char** cmd
- * @Returns: char **
- * @Explanation: gets user input, replaces "\n" with 0 after fgets, and uses replace_Str and split_line to populate cmd.
- */
+* @Function: run_the_shell
+* @Params: char* command_input, char* split_input, char** cmd
+* @Returns: char **
+* @Explanation: gets user input, replaces "\n" with 0 after fgets, and uses replace_Str and split_line to populate cmd.
+*/
 char** run_the_shell(char *command_input, char *split_input, char ** cmd) {
   fgets(command_input, 256, stdin);
   char * newline_char;
@@ -101,11 +101,11 @@ char** run_the_shell(char *command_input, char *split_input, char ** cmd) {
 
 
 /**
- * @Function: check_exit
- * @Params: char * token
- * @Returns: Nothing
- * @Explanation: if the token is "exit" exit the program
- */
+* @Function: check_exit
+* @Params: char * token
+* @Returns: Nothing
+* @Explanation: if the token is "exit" exit the program
+*/
 void check_exit(char* token){
   if(!strcmp(token, "exit")) {
     printf("\nbyebye buddy\n");
@@ -114,11 +114,11 @@ void check_exit(char* token){
 }
 
 /**
- * @Function: print_dir
- * @Params: None
- * @Returns: None
- * @Explanation: Prints the prompt using current working directory
- */
+* @Function: print_dir
+* @Params: None
+* @Returns: None
+* @Explanation: Prints the prompt using current working directory
+*/
 void print_dir() {
   char workingdir[1024];
   getcwd(workingdir,sizeof(workingdir)); //gets current working directory
@@ -126,11 +126,11 @@ void print_dir() {
 }
 
 /**
- * @Function: locate_redirect
- * @Params:
- * @Returns:
- * @Explanation:
- */
+* @Function: locate_redirect
+* @Params:
+* @Returns:
+* @Explanation:
+*/
 int locate_redirect(char ** parsed_line ){
   int red_index=0;
   while(parsed_line[red_index]){
@@ -143,11 +143,11 @@ int locate_redirect(char ** parsed_line ){
 }
 
 /**
- * @Function: redirect
- * @Params:
- * @Returns:
- * @Explanation:
- */
+* @Function: redirect
+* @Params:
+* @Returns:
+* @Explanation:
+*/
 void redirect(char **parsed_line){
   int redirection= locate_redirect(parsed_line);
   if(redirection){
@@ -159,7 +159,7 @@ void redirect(char **parsed_line){
     FILE *newsw;
     int currfr;
     int currfw;
-    if(!strcmp(red,"<")){
+    if(!strcmp(red,"<")) {
       f=fork();
       if(!f){
         red=parsed_line[redirection+1];
@@ -171,102 +171,101 @@ void redirect(char **parsed_line){
           parsed_line[i] = 0;
           i++;
         }
-	int fo = fork();
-	if (!fo){
-	  execvp(parsed_line[0],parsed_line);
-	}else{
-	  int status;
-	  wait(&status);
-	}
-	//execute(parsed_line);
-	dup2(STDIN_FILENO,currfr);
-	close(newfr);
-	exit(0);     
+        int fo = fork();
+        if (!fo){
+          execvp(parsed_line[0],parsed_line);
+        }else{
+          int status;
+          wait(&status);
+        }
+        //execute(parsed_line);
+        dup2(currfr,STDIN_FILENO);
+        //dup2(STDIN_FILENO,currfr);
+        close(newfr);
+        exit(0);
       }
       else{
-	int status;
-	wait(&status);
+        int status;
+        wait(&status);
       }
-    }
-    else if(!strcmp(red,">")){
+    } else if(!strcmp(red,">")) {
       f=fork();
       if(!f){
-	red=parsed_line[redirection+1];
-	newfw=open(red,O_CREAT|O_WRONLY,0644);
-	currfw=dup(1);
-	dup2(newfw,1);
-	int i = redirection;
-	while (parsed_line[i]) { // zero out everything except the command
-	  parsed_line[i] = 0;
-	  i++;
-	}
-	int fo = fork();
-	if (!fo){
-	  execvp(parsed_line[0],parsed_line);
-	}else{
-	  int status;
-	  wait(&status);
-	}
-	//execute(parsed_line);
-	dup2(STDOUT_FILENO,currfw);
-	close(newfw);
-	exit(0);     
+        red=parsed_line[redirection+1];
+        newfw=open(red,O_CREAT|O_WRONLY,0644);
+        currfw=dup(1);
+        dup2(newfw,1);
+        int i = redirection;
+        while (parsed_line[i]) { // zero out everything except the command
+          parsed_line[i] = 0;
+          i++;
+        }
+        int fo = fork();
+        if (!fo){
+          execvp(parsed_line[0],parsed_line);
+        }else{
+          int status;
+          wait(&status);
+        }
+        //execute(parsed_line);
+        dup2(currfw,STDOUT_FILENO);
+        //dup2(STDOUT_FILENO,currfw);
+        close(newfw);
+        exit(0);
       }
       else{
-	int status;
-	wait(&status);
+        int status;
+        wait(&status);
       }
-    }
-    else{
+    } else {
       f=fork();
-      if(!f){
-	char *command0[256];
-	char *command1[256];
-	memset(command0, 0, 256);
-	memset(command1, 0, 256);
-	red=parsed_line[redirection+1];
-	newsr = popen(red,"r");
-	newsw = popen(parsed_line[0],"w");
-	newfr = fileno(newsr);
-	newfw = fileno(newsw);
-	currfr = dup(0);
-	currfw = dup(1);
-	dup2(0,newfr);
-	dup2(1,newfw);
-	int i = 0;
-	while (!strcmp(parsed_line[i], "|")) { // command0 is everything before |
-	  command0[i] = parsed_line[i];
-	  i++;
-	}
-	i++;
-	int shift = i;
-	while (parsed_line[i]) { // command1 is everything after |
-	  command1[i-shift] = parsed_line[i];
-	  i++;
-	}
-	if (!fork()) {
-	  execvp(command1[0],command1);
-	} else {
-	  int status;
-	  wait(&status);
-	}
-	if (!fork()){
-	  execvp(command0[0],command0);
-	}
-	else {
-	  int status;
-	  wait(&status);
-	}
-	dup2(currfr,0);
-	dup2(currfw,1);
-	close(newfr);
-	close(newfw);
-	pclose(newsr);
-	pclose(newsw);
-      }
-      else {
-	int status;
-	wait(&status);
+      if (!f){
+        char *command0[256];
+        char *command1[256];
+        memset(command0, 0, 256);
+        memset(command1, 0, 256);
+        red=parsed_line[redirection+1];
+        newsr = popen(red,"r");
+        newsw = popen(parsed_line[0],"w");
+        newfr = fileno(newsr);
+        newfw = fileno(newsw);
+        currfr = dup(0);
+        currfw = dup(1);
+        dup2(newfr,0);
+        dup2(newfw,1);
+        int i = 0;
+        while (!strcmp(parsed_line[i], "|")) { // command0 is everything before |
+          command0[i] = parsed_line[i];
+          i++;
+        }
+        i++;
+        int shift = i;
+        while (parsed_line[i]) { // command1 is everything after |
+          command1[i-shift] = parsed_line[i];
+          i++;
+        }
+        if (!fork()) {
+          execvp(command0[0],command0);
+        } else {
+          int status0;
+          wait(&status0);
+          if (!fork()){
+            execvp(command1[0],command1);
+          } else {
+            int status1;
+            wait(&status1);
+          }
+        }
+        dup2(currfr,STDIN_FILENO);
+        dup2(currfw,STDOUT_FILENO);
+        close(newfr);
+        close(newfw);
+        pclose(newsr);
+        pclose(newsw);
+        exit(0);
+      } else {
+        int status;
+        wait(&status);
       }
     }
   }
@@ -274,11 +273,11 @@ void redirect(char **parsed_line){
 //copy this for future functions:
 
 /**
- * @Function:
- * @Params:
- * @Returns:
- * @Explanation:
- */
+* @Function:
+* @Params:
+* @Returns:
+* @Explanation:
+*/
 int main() {
   char command_input[256]; // user input string
   char split_cmd[256];     // input string, but with NULLs instead of semicolons
@@ -317,11 +316,10 @@ int main() {
       cd(parsed_line);
       check_exit(cmd[i]);
       if (locate_redirect(parsed_line)){
-	redirect(parsed_line);
-	//print_dir();
-      }
-      else{
-	execute(parsed_line);
+        redirect(parsed_line);
+        //print_dir();
+      } else {
+        execute(parsed_line);
       }
       //print_dir();
       printf("\n--------------------------------\n");
